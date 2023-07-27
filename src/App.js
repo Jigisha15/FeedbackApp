c9d80790663d4f1630fd08393dbd37905edd9e15
Jigisha Manohar <jigishamanohar18@gmail.com>
2023-07-27 23:30:43 +0530

    section 05

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from 'react-router-dom'
 
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import Card from './components/shared/Card'
+import Post from './components/Post'
+
+import AboutPage from './pages/AboutPage'
+
+import FeedbackProvider from './context/FeedbackContext'
 
 import FeedbackData from './data/FeedbackData'
 
@@ -24,21 +37,61 @@ function App() {
   }
 
   return (
-    <>
+    // <FeedbackProvider>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList
-          feedback={feedback}
-          handleDelete={deleteFeedback}
-        />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/about"
+            Component={AboutPage}
+          />
+          <Route
+            path="/post/*"
+            Component={Post}
+          />
+        </Routes>
+
+        {/* <Card>
+          <NavLink
+            to="/"
+            activeClassName="active"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/about"
+            activeClassName="active"
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/post"
+            activeClassName="active"
+          >
+            Post
+          </NavLink>
+        </Card> */}
+
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
+    // </FeedbackProvider>
   )
 }
-// Header.defaultProps = {
-//   text: 'Feedback UI',
-// }
 
 export default App
---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function FeedbackProvider() {
+  return <div></div>
+}
+
+export default FeedbackProvider
